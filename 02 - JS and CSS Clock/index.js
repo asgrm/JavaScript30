00d20b5b96a2dfc71abd7b47e3aabca035685c61
Andrii <andriigromakov@gmail.com>
2021-02-07 11:42:17 +0200

    fix second hand (0sec) bug

--- 02 - JS and CSS Clock/index.js
+++ 02 - JS and CSS Clock/index.js
@@ -6,10 +6,13 @@ function setDate() {
   const seconds = now.getSeconds();
   const minute = now.getMinutes();
   const hours = now.getHours();
-  const secondDeg = (seconds * 6) + 90;
-  const minuteDeg = (minute * 6) + 90;
-  const hourDeg = (hours * 30) + 90;
+  const secondDeg = (seconds * 6);
+  const minuteDeg = (minute * 6);
+  const hourDeg = (hours * 30);
   
+  if (seconds === 0) secondHand.classList.add('nottransition');
+  if (seconds === 1) secondHand.classList.remove('nottransition');
+
   secondHand.style.transform = `rotate(${secondDeg}deg)`;
   minuteHand.style.transform = `rotate(${minuteDeg}deg)`;
   hourHand.style.transform = `rotate(${hourDeg}deg)`;
