4008cd9c80187df83be563bcc89d50496dc852d4
Andrii <andriigromakov@gmail.com>
2021-02-08 12:04:51 +0200

    disable multiple panels opening

--- 05 - Flex Panel Gallery/index.js
+++ 05 - Flex Panel Gallery/index.js
@@ -1,11 +1,21 @@
 const panels = document.querySelectorAll('.panel');
 
 function toggleOpen(){
+  panels.forEach(elem => {
+    if (this !== elem) {
+      elem.classList.remove('open')
+    }
+  })
   this.classList.toggle('open');
 }
 
 function toggleActive(e) {
   if(e.propertyName.includes('flex')) {
+  panels.forEach(elem => {
+    if (this !== elem) {
+      elem.classList.remove('open-active')
+    }
+  })
   this.classList.toggle('open-active');
   }
 }
