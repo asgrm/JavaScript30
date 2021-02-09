335146512f57a2c4fa0a8e1968bdc2b37108423e
Andrii <andriigromakov@gmail.com>
2021-02-09 11:03:36 +0200

    variable lastCkecked does not change if checkbox was unchecked , add logic to uncheck

--- 10 - Hold Shift and Check Checkboxes/index.js
+++ 10 - Hold Shift and Check Checkboxes/index.js
@@ -1,20 +1,28 @@
 const checkboxes = document.querySelectorAll('.item input[type="checkbox"]');
 
 let lastChecked;
+let lastUnchecked;
 let inBetween;
 
 function handleCheckbox(e) {
   if (e.shiftKey && this.checked) {
-
     checkboxes.forEach(elem => {
-      console.log(elem);
       if (inBetween) elem.checked = true;
       if (elem === lastChecked || elem === this) {
         inBetween = !inBetween;
       }
     })
   }
-  lastChecked = this;
+  if (e.shiftKey && !this.checked) {
+    checkboxes.forEach(elem => {
+      if (inBetween) elem.checked = false;
+      if (elem === lastUnchecked || elem === this) {
+        inBetween = !inBetween;
+      }
+    })
+  }
+  if (this.checked) lastChecked = this;
+  if (!this.checked) lastUnchecked = this;
 }
 
 checkboxes.forEach(elem => elem.addEventListener('click', handleCheckbox));
