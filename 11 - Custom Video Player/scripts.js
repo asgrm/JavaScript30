c6fbb4529f41c788efebb717e2aa3e7b3a381e92
Andrii <andriigromakov@gmail.com>
2021-02-09 17:49:43 +0200

    add fullscreen

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -5,8 +5,12 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+const fullScreen = player.querySelector('.player__fullscreen');
+const iconFull = player.querySelector('.icon-full');
+const iconShrink = player.querySelector('.icon-shrink');
 
 let mousedown = false;
+let isFullScreen = false;
 
 function togglePlay() {
   video.paused ? video.play() : video.pause();
@@ -33,6 +37,38 @@ function scrub(e) {
   video.currentTime = (e.offsetX / progress.offsetWidth) * video.duration;
 }
 
+function handleFullscreen () {
+
+  if (!isFullScreen) {
+    iconShrink.style.display = "inline";
+    iconFull.style.display = "none"
+    if (player.requestFullscreen) {
+      player.requestFullscreen();
+    } else if (player.mozRequestFullScreen) {
+      player.mozRequestFullScreen();
+    } else if (player.webkitRequestFullscreen) {
+      player.webkitRequestFullscreen();
+    } else if (player.msRequestFullscreen) {
+      player.msRequestFullscreen();
+    }
+  } else {
+    iconShrink.style.display = "none";
+    iconFull.style.display = "inline";
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitCancelFullScreen) {
+      document.webkitCancelFullScreen();
+    } else if (player.mozCancelFullScreen) {
+      player.mozCancelFullScreen();
+    } else if (player.msCancelFullScreen) {
+      player.msCancelFullScreen();
+    }
+  }
+  
+  isFullScreen = !isFullScreen;
+
+}
+
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateBtn);
 video.addEventListener('pause', updateBtn);
@@ -48,3 +84,5 @@ progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
 progress.addEventListener('mousedown', () => mousedown = true);
 progress.addEventListener('mouseup', () => mousedown = false);
+
+fullScreen.addEventListener("click", handleFullscreen);
