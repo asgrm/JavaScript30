d1ce828a3e953ef6039ada6a8403f28aa1df8927
Andrii <andriigromakov@gmail.com>
2021-02-12 15:03:57 +0200

    add #20 speech recognition

---
+++ 20 - Speech Detection/scripts.js
@@ -0,0 +1,34 @@
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+recognition.lang = 'en-US';
+
+let p = document.createElement('p');
+const words = document.querySelector('.words');
+words.append(p);
+
+recognition.addEventListener('result', e => {
+  const transcript = Array.from(e.results)
+  .map(result => result[0])
+  .map(result => result.transcript)
+  .join('');
+  p.textContent = transcript;
+
+  if (e.results[0].isFinal) {
+    p = document.createElement('p');
+    words.append(p);
+  }
+
+    
+  if (transcript.includes('unicorn')) {
+    console.log('🦄🦄🦄🦄');
+  }
+
+  
+  console.log(transcript);
+})
+
+
+recognition.addEventListener('end', recognition.start);
+recognition.start();
