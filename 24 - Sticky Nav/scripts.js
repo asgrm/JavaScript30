c8e90c455d3efbe34332befd126d5372a1736f62
Andrii <andriigromakov@gmail.com>
2021-02-21 12:02:46 +0200

    add slide-in effect(adjusted values in checkSlide)

--- 24 - Sticky Nav/scripts.js
+++ 24 - Sticky Nav/scripts.js
@@ -1,9 +1,9 @@
 const nav = document.querySelector('#main');
+const header = document.querySelector('header');
 const topOfNav = nav.offsetTop;
-
+const sliderImages = document.querySelectorAll('.slide-in');
 
 function fixNav() {
-  console.log(topOfNav, window.scrollY);
   if(window.scrollY >= topOfNav) {
     document.body.style.paddingTop = nav.offsetHeight + 'px';
     document.body.classList.add('fixed-nav');
@@ -13,4 +13,29 @@ function fixNav() {
   }
 }
 
+function checkSlide () {
+  sliderImages.forEach(slide => {
+    const slideInAt = (window.scrollY + window.innerHeight - nav.offsetHeight - header.offsetHeight) - (slide.height / 2);
+    const slideBottom = slide.offsetTop + slide.height;
+    const isHalfShown = slideInAt > slide.offsetTop;
+    const isNotScrollPassed = window.scrollY < (slideBottom + nav.offsetHeight + header.offsetHeight);
+    if (isHalfShown && isNotScrollPassed) {
+      slide.classList.add('active');
+    } else {
+      slide.classList.remove('active');
+    }
+  })
+}
+
+function debounce(f, ms=50) {
+  let isCooldown = false;
+  return function() {
+    if (isCooldown) return;
+    f.apply(this, arguments);
+    isCooldown = true;
+    setTimeout(() => isCooldown = false, ms);
+  };
+}
+
 window.addEventListener('scroll', fixNav);
+window.addEventListener('scroll', debounce(checkSlide));
