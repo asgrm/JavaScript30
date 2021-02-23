248f7fc4b78d504706be9f3df23b5bf4ec9176a2
Andrii <andriigromakov@gmail.com>
2021-02-23 12:40:09 +0200

    removed uncheck logic

--- 10 - Hold Shift and Check Checkboxes/index.js
+++ 10 - Hold Shift and Check Checkboxes/index.js
@@ -1,7 +1,6 @@
 const checkboxes = document.querySelectorAll('.item input[type="checkbox"]');
 
 let lastChecked;
-let lastUnchecked;
 let inBetween;
 
 function handleCheckbox(e) {
@@ -13,16 +12,7 @@ function handleCheckbox(e) {
       }
     })
   }
-  if (e.shiftKey && !this.checked) {
-    checkboxes.forEach(elem => {
-      if (inBetween) elem.checked = false;
-      if (elem === lastUnchecked || elem === this) {
-        inBetween = !inBetween;
-      }
-    })
-  }
   if (this.checked) lastChecked = this;
-  if (!this.checked) lastUnchecked = this;
 }
 
 checkboxes.forEach(elem => elem.addEventListener('click', handleCheckbox));
