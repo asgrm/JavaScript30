c814141ff47c96155479fbc73341de5cf6e1e11c
Andrii <andriigromakov@gmail.com>
2021-02-23 14:34:35 +0200

    add styling to #18

--- 18 - Adding Up Times with Reduce/scripts.js
+++ 18 - Adding Up Times with Reduce/scripts.js
@@ -1,5 +1,10 @@
 const timeNodes = document.querySelectorAll('[data-time]');
 
+Array.from(timeNodes).forEach(elem => {
+  const a = `${elem.textContent} ${elem.dataset.time}`;
+  elem.textContent = a;
+})
+
 const seconds = Array.from(document.querySelectorAll('[data-time]'), node => node.dataset.time)
 .reduce((acc, cur) => {
   const [min, sec] = cur.split(':');
@@ -12,4 +17,8 @@ secondsLeft = secondsLeft % 3600;
 const minutes = Math.floor(secondsLeft / 60);
 secondsLeft = secondsLeft % 60;
 
-console.log(hours, minutes, secondsLeft);
+const formattedTimeString = `${hours < 10 ? '0' : ''}${hours}:${minutes < 10 ? '0' : ''}${minutes}:${secondsLeft < 10 ? '0' : ''}${secondsLeft}`;
+
+const display = document.createElement('p');
+display.textContent = `Total: ${formattedTimeString}`;
+document.body.append(display);
