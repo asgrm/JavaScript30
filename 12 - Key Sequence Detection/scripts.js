c3d6ecdd38251c25ea08510dad2f860a4d53e543
Andrii <andriigromakov@gmail.com>
2021-02-23 12:53:30 +0200

    add some styling, confetti instead unicorns

--- 12 - Key Sequence Detection/scripts.js
+++ 12 - Key Sequence Detection/scripts.js
@@ -1,12 +1,13 @@
 const pressed = []; 
-const secretcode = 'wesbos';
+const secretcode = 'confetti';
 
 window.addEventListener('keyup', (e) => {
   pressed.push(e.key);
   pressed.splice(-secretcode.length - 1, pressed.length - secretcode.length);
   if (pressed.join('').includes(secretcode)) {
-    console.log('!!!');
-    cornify_add();
+    confetti.start()
+    setTimeout(() => confetti.stop(), 2000);
+    
   }
   console.log(pressed);
 });
